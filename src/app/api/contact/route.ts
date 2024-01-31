22448c198878fb79426bb6badf8f62e53455bbde
Dmytro <dmitry.yurin2020@gmail.com>
2024-01-31 22:01:23 +0200

    add options for contact route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -114,3 +114,16 @@ export async function GET(req: Request) {
     return new Response(JSON.stringify(contact));
   }
 }
+
+export async function OPTIONS(request: Request) {
+  const origin = request.headers.get("origin");
+
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": origin || "*",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
